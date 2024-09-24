74d99e93b321aee84f041f4e0948af58df3d5424
sgzh1 <ya.superkesa@gmail.com>
2024-09-24 11:21:52 +0300

    refactored code of v1: finish

--- v1/src/order_fulfillment_forecast_requires_v1.py
+++ v1/src/order_fulfillment_forecast_requires_v1.py
@@ -1,24 +1,42 @@
+import os
 from flask import Flask, request, jsonify
 import joblib
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.multioutput import MultiOutputRegressor
+from sklearn.preprocessing import LabelEncoder
 
 app = Flask(__name__)
 
 # Load the trained model and label encoders
-model = joblib.load('/Users/sgzh1/projects/order-fulfillment-forecast-requires/v1/model/modelorder_fulfillment_model.pkl')
-label_encoders = joblib.load('/Users/sgzh1/projects/order-fulfillment-forecast-requires/v1/model/modellabel_encoders.pkl')
+model_path = '/Users/sgzh1/projects/order-fulfillment-forecast-requires/v1/model/modelorder_fulfillment_model.pkl'
+label_encoders_path = '/Users/sgzh1/projects/order-fulfillment-forecast-requires/v1/model/modellabel_encoders.pkl'
+
+if os.path.exists(model_path) and os.path.exists(label_encoders_path):
+    model = joblib.load(model_path)
+    label_encoders = joblib.load(label_encoders_path)
+else:
+    model = MultiOutputRegressor(RandomForestRegressor(n_estimators=100))
+    label_encoders = {'type_encoder': None, 'pm_encoder': None}
+
+def safe_transform(label_encoder, value, default_value):
+    """
+    Safely transform a value using a LabelEncoder.
+    If the value is not in the encoder's classes_, return a default value.
+    """
+    if value in label_encoder.classes_:
+        return label_encoder.transform([value])[0]
+    else:
+        return label_encoder.transform([default_value])[0]
 
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json(force=True)
     
-    # Encode the input data
-    type_encoded = label_encoders['type_encoder'].transform([data['type']])[0]
-    pm_encoded = label_encoders['pm_encoder'].transform([data['processing_method']])[0]
+    # Encode the input data, handling unknown values
+    type_encoded = safe_transform(label_encoders['type_encoder'], data['type'], label_encoders['type_encoder'].classes_[0])
+    pm_encoded = safe_transform(label_encoders['pm_encoder'], data['processing_method'], label_encoders['pm_encoder'].classes_[0])
     
     # Prepare the input for the model
     input_data = np.array([[type_encoded, pm_encoded, data['sku_id']]])
@@ -43,21 +61,16 @@ def fine_tune():
     # Load new data from the CSV file
     new_data = pd.read_csv(file_path)
 
-    # Preprocess the new data (similar to initial training)
-    new_data['type_encoded'] = label_encoders['type_encoder'].transform(new_data['type'])
-    new_data['processing_method_encoded'] = label_encoders['pm_encoder'].transform(new_data['processing_method'])
+    # Preprocess the new data (similar to initial training), handling unknown values
+    new_data['type_encoded'] = new_data['type'].apply(lambda x: safe_transform(label_encoders['type_encoder'], x, label_encoders['type_encoder'].classes_[0]))
+    new_data['processing_method_encoded'] = new_data['processing_method'].apply(lambda x: safe_transform(label_encoders['pm_encoder'], x, label_encoders['pm_encoder'].classes_[0]))
 
     # Features and target variables
     X_new = new_data[['type_encoded', 'processing_method_encoded', 'sku_id']]
     y_new = new_data[['op_pallet_pick', 'op_cont_pick', 'op_cont_deliver', 'op_cont_aboard', 'op_load_deliver']]
 
-    # Combine old and new data for retraining
-    X_old, y_old = model.estimator_.estimators_[0].tree_.feature_importances_.reshape(-1, 3), model.estimator_.estimators_[0].tree_.value
-    X_combined = np.vstack((X_old, X_new))
-    y_combined = np.vstack((y_old, y_new))
-
-    # Retrain the model
-    model.fit(X_combined, y_combined)
+    # Retrain the model with new data
+    model.fit(X_new, y_new)
 
     # Save the updated model
     joblib.dump(model, '/Users/sgzh1/projects/order-fulfillment-forecast-requires/v1/model/modelorder_fulfillment_model.pkl')
